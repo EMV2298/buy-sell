1af5a99777ef093e4bcec1e353c79763ca384432
EMV2298 <emv2298@icloud.com>
2023-02-16 00:06:23 +0300

    delete unused

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -3,13 +3,9 @@
 namespace app\controllers;
 
 use app\models\form\Register;
-use app\models\Users;
 use app\src\factory\UserFactory;
-use app\src\service\UploadFile;
 use Yii;
 use yii\base\Controller;
-use yii\rbac\DbManager;
-use yii\web\ServerErrorHttpException;
 use yii\web\UploadedFile;
 
 class RegisterController extends Controller
